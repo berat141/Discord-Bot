01c20a47ce73a4509702dc812e15a8ad51878a0c
berat141 <beratozen5656@gmail.com>
2023-08-09 18:36:29 +0300

    Update ticketOpenButton.js

--- events/interactionCreate/buttons/ticketOpenButton.js
+++ events/interactionCreate/buttons/ticketOpenButton.js
@@ -3,31 +3,27 @@ const { ModalBuilder,TextInputBuilder,ActionRowBuilder, TextInputStyle} = requir
 module.exports = async (interaction) => {
   if(interaction.customId != "open") return;
   const modal = new ModalBuilder()
-			.setCustomId('modal')
-			.setTitle('Sorularımız');
+	        .setCustomId('modal')
+		.setTitle('Sorularımız');
 
-		const gmail = new TextInputBuilder()
-			.setCustomId('mail')
-			.setLabel("Mailiniz")
-      .setPlaceholder("...@gmail.com")
+  const gmail = new TextInputBuilder()
+		.setCustomId('mail')
+		.setLabel("Mailiniz")
+                .setPlaceholder("...@gmail.com")
   		.setStyle(TextInputStyle.Short)
-      .setRequired(true)
+                .setRequired(true)
 
-		const sorun = new TextInputBuilder()
-			.setCustomId('sorun')
-			.setLabel("Sorununuz Nedir?")
-      .setPlaceholder("Lütfen sorununuzu bizle paylaşın")
-      .setRequired(true)
-//      .setValue('Default')
-			.setStyle(TextInputStyle.Paragraph)
-      .setMaxLength(1024)
+  const sorun = new TextInputBuilder()
+		.setCustomId('sorun')
+		.setLabel("Sorununuz Nedir?")
+                .setPlaceholder("Lütfen sorununuzu bizle paylaşın")
+                .setRequired(true)
+		.setStyle(TextInputStyle.Paragraph)
+                .setMaxLength(1024)
 
-		const firstActionRow = new ActionRowBuilder().addComponents(gmail);
-		const secondActionRow = new ActionRowBuilder().addComponents(sorun);
-
+  const firstActionRow = new ActionRowBuilder().addComponents(gmail);
+  const secondActionRow = new ActionRowBuilder().addComponents(sorun);
 	
-		modal.addComponents(firstActionRow, secondActionRow);
-
-	
-		await interaction.showModal(modal);
+  modal.addComponents(firstActionRow, secondActionRow);
+  await interaction.showModal(modal);
 }
