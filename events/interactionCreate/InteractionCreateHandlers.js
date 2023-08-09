8a55c394534044dd622bf0ede11319a1b8e1488c
berat141 <beratozen5656@gmail.com>
2023-08-09 18:24:23 +0300

    Create InteractionCreateHandlers.js

---
+++ events/interactionCreate/InteractionCreateHandlers.js
@@ -0,0 +1,10 @@
+module.exports = {
+  buttons:{
+    ticketOpenButton: require("./buttons/ticketOpenButton"),
+    ticketCloseButton:require("./buttons/ticketCloseButton")
+  },
+  modals:{
+    ticketOpenModal:require("./modals/ticketOpenModal"),
+    ticketCloseModal:require("./modals/ticketCloseModal")
+  }
+}
