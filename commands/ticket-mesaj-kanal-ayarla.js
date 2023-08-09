0e25d1d5b3b4f4813e58328a9a36890b01e95d7a
berat141 <beratozen5656@gmail.com>
2023-08-10 01:44:11 +0300

    Update ticket-mesaj-kanal-ayarla.js

--- commands/ticket-mesaj-kanal-ayarla.js
+++ commands/ticket-mesaj-kanal-ayarla.js
@@ -1,13 +1,15 @@
-const { SlashCommandBuilder, PermissionFlagsBits,ChannelType,EmbedBuilder} = require("discord.js")
+const { SlashCommandBuilder,PermissionFlagsBits,ChannelType,EmbedBuilder } = require("discord.js")
 
 module.exports.run = async (interaction,db) => {
   const channel = interaction.options.getChannel('kanal')
   const guild = interaction.guild
-  //console.log(guild)
-  //console.log(data)
-    await db.set(`ticketKanal_${guild.id}`, channel.id)
-    const embed = new EmbedBuilder().setTitle("Kanal Ayarlandı").setDescription(`\` Kanal başarıyla ayarlandı! \`\n\n**Kanal:**<#${channel.id}>}**`).setColor('Green')
-    interaction.reply({embeds:[embed]})
+  
+  await db.set(`ticketKanal_${guild.id}`, channel.id)
+  const embed = new EmbedBuilder()
+                .setTitle("Kanal Ayarlandı")
+                .setDescription(`\` Kanal başarıyla ayarlandı! \`\n\n**Kanal:**<#${channel.id}>}**`)
+                .setColor('Green')
+  interaction.reply({embeds:[embed]})
 }
 
 module.exports.info = {
@@ -23,5 +25,5 @@ module.exports.info = {
          .setDescription('d')
          .setRequired(true)
          .addChannelTypes(ChannelType.GuildText))
-         .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
 }
